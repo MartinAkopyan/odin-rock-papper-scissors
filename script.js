79367a6d1ad0188f78c4f6d593bb119bc061a4af
mrtn <martinakopyan34@gmail.com>
2023-04-18 19:17:50 +0300

    add logic to cards | add restart function

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let choices = ['rock', 'paper', 'scissors']
 let playerSelection;
 let computerSelection;
+const restartBtn = document.querySelector('.btn');
+const playerPoints = document.querySelector('#playerScore');
+const computerPoints = document.querySelector('#pcScore');
+const text = document.querySelector('#text');
+const cards = document.querySelectorAll('.cards__card');
 
 function getComputerChoice() {
 	let randomChoice = Math.floor(Math.random() * 3);
@@ -10,47 +15,84 @@ function getComputerChoice() {
 let playerScore = 0;
 let computerScore = 0;
 
+playerPoints.textContent = playerScore;
+computerPoints.textContent = computerScore;
+
+cards.forEach(card => {
+	card.addEventListener('click', (e) => {
+		playerSelection = e.currentTarget.dataset.value;
+	})
+})
+
+
 function playRound(playerSelection, computerSelection) {
 	computerSelection = getComputerChoice();
-	playerSelection = prompt('Rock, paper, scissors ?');
-	if (playerSelection.trim().toLowerCase() === computerSelection) {
+
+	if (playerSelection === computerSelection) {
 		console.log('')
 		console.log('PS: ', playerScore);
 		console.log('PcS: ', computerScore);
-		return 'Draw!';
+		text.textContent = 'It\'s a tie!';
 	}
 
 	if (
-		(playerSelection.trim().toLowerCase() === 'rock' && computerSelection === 'scissors') ||
-		(playerSelection.trim().toLowerCase() === 'scissors' && computerSelection === 'paper') ||
-		(playerSelection.trim().toLowerCase() === 'paper' && computerSelection === 'rock')
-		) {
+		(playerSelection === 'rock' && computerSelection === 'scissors') ||
+		(playerSelection === 'scissors' && computerSelection === 'paper') ||
+		(playerSelection === 'paper' && computerSelection === 'rock')
+	) {
 		console.log('------')
+		if (playerScore > 4) {
+			console.log(11)
+			text.textContent = 'Great! You won the game!';
+		}
 		++playerScore;
 		console.log('Your score: ', playerScore);
 		console.log('Computer score: ', computerScore);
-		return `You won, ${playerSelection} beats ${computerSelection}!`
+		text.textContent = `You won, ${playerSelection} beats ${computerSelection}!`
 	}
 
 	if (
-		(playerSelection.trim().toLowerCase() === 'rock' && computerSelection === 'paper') ||
-		(playerSelection.trim().toLowerCase() === 'paper' && computerSelection === 'scissors') ||
-		(playerSelection.trim().toLowerCase() === 'scissors' && computerSelection === 'rock')
-		) {
+		(playerSelection === 'rock' && computerSelection === 'paper') ||
+		(playerSelection === 'paper' && computerSelection === 'scissors') ||
+		(playerSelection === 'scissors' && computerSelection === 'rock')
+	) {
 		console.log('------')
+		if (computerScore > 4) {
+			console.log(12)
+			text.textContent = 'Sorry! Yoy lost the game!';
+		}
 		++computerScore;
 		console.log('Your score: ', playerScore);
 		console.log('Computer score: ', computerScore);
-		return `You've lost this round, ${computerSelection} beats ${playerSelection}!`
+		text.textContent = `You've lost this round, ${computerSelection} beats ${playerSelection}!`
 	}
+
+	playerPoints.textContent = playerScore;
+	computerPoints.textContent = computerScore;
 }
 
 function game() {
-	for (let i = 0; playerScore < 3 && computerScore < 3; i++) {
-		console.log(playRound(playerSelection, computerSelection))
-	}
-
-	playerScore > computerScore ? console.log('Great! You won the game!') : console.log('Sorry! Yoy lost the game!');
+	cards.forEach(card => {
+		card.addEventListener('click', function () {
+			if (playerScore > 4 || computerScore > 4) {
+				return;
+			} else {
+				playRound(playerSelection, computerSelection)
+			}
+		});
+	})
 }
 
 game();
+
+restartBtn.addEventListener('click', restart)
+
+function restart() {
+	console.log('11')
+	playerScore = 0;
+	computerScore = 0;
+
+	playerPoints.textContent = playerScore;
+	computerPoints.textContent = computerScore;
+	text.textContent = 'Let\'s start our game! Make a choice.'
+}
